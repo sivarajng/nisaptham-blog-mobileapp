c14573664b634a2c29045799e511ffd0e06ca485
Sivaraj Nagaraj <sivaraj.nagaraj@BRILLIO.COM>
2017-07-24 16:41:27 +0530

    -test

--- app/redux/reducers/text.js
+++ app/redux/reducers/text.js
@@ -3,8 +3,7 @@ import Type from '../actionTypes'
 export default (state={}, { type, payload }) => {
 
     switch (type) {
-        case Type.GET:
-            return { ...state, Data: payload };
+     
 
         default:
             return state;
