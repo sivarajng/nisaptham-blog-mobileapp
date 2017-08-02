e9da46c1250e7059d5ffe86e3cc864a1ce1a7c2c
Sivaraj Nagaraj <sivaraj.nagaraj@BRILLIO.COM>
2017-08-02 14:08:07 +0530

    post list

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Text,
   View,
-  ListView
+  ListView,
+  TouchableOpacity
 } from 'react-native';
 
 import { connect } from 'react-redux'
@@ -28,59 +29,37 @@ class Home extends Component {
 
   renderRow(item) {
     return (
-      <Card>
-        <CardSection>
-          <View style={styles.productDtailsCont}>
-            <Text style={[styles.productDetailsText, commonStyles.opensansBold, commonStyles.fontSize16]}>{item.title.$t}</Text>
-            <Text style={[styles.content, commonStyles.opensansRegular, commonStyles.fontSize14]}>
-           {item.summary.$t}
-             </Text>
-          </View>
+      <TouchableOpacity onPress={() => Actions.Post()}>
+        <Card>
+          <CardSection>
+            <View style={styles.productDtailsCont}>
+              <Text style={[styles.productDetailsText, commonStyles.opensansBold, commonStyles.fontSize16]}>{item.title.$t}</Text>
+              <Text style={[styles.content, commonStyles.opensansRegular, commonStyles.fontSize14]}>
+                {item.summary.$t}
+              </Text>
+            </View>
 
-        </CardSection>
-      </Card>
+          </CardSection>
+        </Card>
+      </TouchableOpacity>
     )
   }
   render() {
-    console.log('lllllllll ',this.props.posts);
+    console.log('lllllllll ', this.props.posts);
     return (
       <View style={styles.container}>
         <Text onPress={() => Actions.Post()}
           style={styles.welcome}>
           Welcome to React Native! HOME SRISJ!!!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        {this.props.posts.feed
+          ? <ListView
 
-      {this.props.posts.feed
-        ?<ListView
-
-          enableEmptySections={true}
-          dataSource={this.ds.cloneWithRows(this.props.posts.feed.entry)}
-          renderRow={this.renderRow.bind(this)}
-        />
-        :null}
-
-        <Card>
-          <CardSection>
-            <View style={styles.productDtailsCont}>
-              <Text style={[styles.productDetailsText, commonStyles.opensansBold, commonStyles.fontSize16]}>Product Details</Text>
-              <Text style={[styles.content, commonStyles.opensansRegular, commonStyles.fontSize14]}>
-                The standard chunk of Lorem Ipsum used since the 1500s is
-                                        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"
-                                        by Cicero are also reproduced in their exact original form,
-                                        accompanied by English versions from the 1914 translation by H. Rackham.
-                                    </Text>
-            </View>
-
-          </CardSection>
-        </Card>
-
+            enableEmptySections={true}
+            dataSource={this.ds.cloneWithRows(this.props.posts.feed.entry)}
+            renderRow={this.renderRow.bind(this)}
+          />
+          : null}
 
       </View>
     );
